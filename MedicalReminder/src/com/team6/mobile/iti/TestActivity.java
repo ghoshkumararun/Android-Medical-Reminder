71bc3dd73ef3767f036ea115908347c87703fc6f
Mohamed Gaber <m.gaber.ali92@gmail.com>
2015-03-09 17:17:32 +0200

    Add AddMedicine Activity

---
+++ MedicalReminder/src/com/team6/mobile/iti/TestActivity.java
@@ -0,0 +1,16 @@
+package com.team6.mobile.iti;
+import com.team6.mobile.iti.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class TestActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_medicine);
+	}
+
+}
